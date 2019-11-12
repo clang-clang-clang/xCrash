4ffbf0e92aa3066273582d9ee46896bb1762c189
Ta-da <53489032+clang-clang-clang@users.noreply.github.com>
2019-11-12 10:32:58 +0800

    [debug] demo for double catches iqiyi/xCrash#24

--- src/java/xcrash/xcrash_sample/src/main/java/xcrash/sample/MyService.java
+++ src/java/xcrash/xcrash_sample/src/main/java/xcrash/sample/MyService.java
@@ -3,19 +3,33 @@ package xcrash.sample;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import xcrash.XCrash;
 
 public class MyService extends Service {
+    private final String TAG = "DoubleCatch";
+    private int constructCnt = 0;
+    private int createCnt = 0;
+    private int startCommandCnt = 0;
+    private int bindCnt = 0;
+
     public MyService() {
+        Log.d(TAG, "MyService: constructCnt is " + constructCnt++);
+        Log.d(TAG, "MyService: thread is " + Thread.currentThread());
+        Log.d(TAG, "MyService: object is " + this.hashCode());
     }
 
     @Override
     public void onCreate() {
+        Log.d(TAG, "MyService: createCnt is " + createCnt++);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "MyService: startCommandCnt is " + startCommandCnt++);
+        Log.d(TAG, "MyService: Process is " + android.os.Process.myPid());
+
         String type = intent.getStringExtra("type");
         if (type != null) {
             if (type.equals("native")) {
@@ -29,6 +43,13 @@ public class MyService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d(TAG, "MyService: bindCnt is " + bindCnt++);
         return null;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        Log.d(TAG, "MyService: invoke finalize");
+        super.finalize();
     }
 }
